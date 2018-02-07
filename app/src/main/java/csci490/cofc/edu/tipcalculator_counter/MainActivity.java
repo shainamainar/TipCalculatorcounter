86f3c82f4c881098e8bdcfae240783f1f591713e
Shaina Mae Mainar <shainamainar@gmail.com>
2018-02-07 17:16:09 -0500

    update 2

--- app/src/main/java/csci490/cofc/edu/tipcalculator_counter/MainActivity.java
+++ app/src/main/java/csci490/cofc/edu/tipcalculator_counter/MainActivity.java
@@ -2,6 +2,7 @@ package csci490.cofc.edu.tipcalculator_counter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void fifteenClicked(View view) {
+
+    }
+
+    public void eighteenClicked(View view) {
+    }
+
+    public void twentyClicked(View view) {
+    }
+
+    public void otherClicked(View view) {
     }
 }
